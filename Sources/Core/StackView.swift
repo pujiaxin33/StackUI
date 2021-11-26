d47b2d8eafa588e67d65e6cd51fdfedfd026e40e
暴走的鑫鑫 <317437084@qq.com>
2021-11-26 20:07:05 +0800

    update to 0.0.9

--- Sources/Core/StackView.swift
+++ Sources/Core/StackView.swift
@@ -7,6 +7,34 @@
 
 import UIKit
 
+@resultBuilder
+public struct ViewBuilder {
+    public static func buildExpression(_ expression: UIView) -> [UIView] {
+        return [expression]
+    }
+    public static func buildExpression(_ expression: ()) -> [UIView] {
+        return []
+    }
+    public static func buildBlock(_ components: [UIView]...) -> [UIView] {
+        return components.flatMap { $0 }
+    }
+    public static func buildBlock(_ components: UIView...) -> [UIView] {
+        return components.map { $0 }
+    }
+    public static func buildIf(_ component: [UIView]?) -> [UIView] {
+        return component ?? []
+    }
+    public static func buildEither(first component: [UIView]) -> [UIView] {
+        return component
+    }
+    public static func buildEither(second component: [UIView]) -> [UIView] {
+        return component
+    }
+    public static func buildArray(_ components: [[UIView]]) -> [UIView] {
+        Array(components.joined())
+    }
+}
+
 open class HStack: UIStackView, StackUIView {
     public convenience init(distribution: UIStackView.Distribution = .fill, alignment: UIStackView.Alignment = .fill, spacing: CGFloat = 0, @ViewBuilder views: () -> [UIView]) {
         let views = views()
