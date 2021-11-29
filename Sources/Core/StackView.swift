0c9054335b429e4cb0631c8ab598796edad45522
暴走的鑫鑫 <317437084@qq.com>
2021-11-29 09:57:06 +0800

    ViewBuilder update

--- Sources/Core/StackView.swift
+++ Sources/Core/StackView.swift
@@ -9,28 +9,27 @@ import UIKit
 
 @resultBuilder
 public struct ViewBuilder {
-    public static func buildExpression(_ expression: UIView) -> [UIView] {
+    public typealias Expression = UIView
+    public typealias Component = [UIView]
+    public static func buildExpression(_ expression: Expression) -> Component {
         return [expression]
     }
-    public static func buildExpression(_ expression: ()) -> [UIView] {
-        return []
-    }
-    public static func buildBlock(_ components: [UIView]...) -> [UIView] {
+    public static func buildBlock(_ components: Component...) -> Component {
         return components.flatMap { $0 }
     }
-    public static func buildBlock(_ components: UIView...) -> [UIView] {
+    public static func buildBlock(_ components: UIView...) -> Component {
         return components.map { $0 }
     }
-    public static func buildIf(_ component: [UIView]?) -> [UIView] {
+    public static func buildOptional(_ component: Component?) -> Component {
         return component ?? []
     }
-    public static func buildEither(first component: [UIView]) -> [UIView] {
+    public static func buildEither(first component: Component) -> Component {
         return component
     }
-    public static func buildEither(second component: [UIView]) -> [UIView] {
+    public static func buildEither(second component: Component) -> Component {
         return component
     }
-    public static func buildArray(_ components: [[UIView]]) -> [UIView] {
+    public static func buildArray(_ components: [Component]) -> Component {
         Array(components.joined())
     }
 }
